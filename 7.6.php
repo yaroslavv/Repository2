291f41391973b2782ed4de2a5ecf843723606432
yaroslavv <40766321+yaroslavv@users.noreply.github.com>
2018-07-02 11:55:44 -0400

    Add files via upload

---
+++ 7.6.php
@@ -0,0 +1,30 @@
+<?php
+
+$family = array("Rob", "Kirsten", "Tommy", "Ralphie");
+
+foreach ($family as $key => $value) {
+    
+    $family[$key] = $value." Percival";
+    
+    echo "Array item ".$key." is ".$value."<br>";
+    
+}
+
+
+
+for ($i = 0; $i < sizeof($family); $i++) {
+    
+    echo $family[$i]."<br>";
+    
+}
+
+
+
+for ($i = 10; $i >= 0; $i--) {
+    
+    echo $i."<br>";
+    
+}
+
+
+?>
