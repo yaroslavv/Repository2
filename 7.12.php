a6dccfc8be9d67c5ca282a845a8eb95c15331faf
yaroslavv <40766321+yaroslavv@users.noreply.github.com>
2018-07-02 13:26:10 -0400

    Update 7.12.php

--- 7.12.php
+++ 7.12.php
@@ -2,6 +2,6 @@
 
     include("includedfile.php");
     
-    echo file_get_contents("https://www.ecowebhosting.co.uk");
+    echo file_get_contents("https://www.webhosting.com");//get file content from host website
 
 ?>
