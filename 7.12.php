8fc3736e0f3ad4f69af2179dd04ac95ebbfccfee
yaroslavv <40766321+yaroslavv@users.noreply.github.com>
2018-07-02 13:26:32 -0400

    Update 7.12.php

--- 7.12.php
+++ 7.12.php
@@ -2,6 +2,6 @@
 
     include("includedfile.php");
     
-    echo file_get_contents("https://www.webhosting.com");//get file content from host website
+    echo file_get_contents("https://www.webhosting.com");//get file content from host website-requires a specific directory
 
 ?>
