b78645910ff5b2ce80017ebe2f18c4b0bfedc806
yaroslavv <40766321+yaroslavv@users.noreply.github.com>
2018-07-02 14:41:47 -0400

    Update and rename 7.12.php to GetContentSnnipet.php

--- 7.12.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-    include("includedfile.php");
-    
-    echo file_get_contents("https://www.webhosting.com");//get file content from host website-requires a specific directory
-
-?>
---
+++ GetContentSnnipet.php
@@ -0,0 +1,7 @@
+<?php
+
+    include("includedfile.php");
+    
+    echo file_get_contents("https://www.Mywebhosting.com");//get file content from host website-requires a specific directory
+
+?>
